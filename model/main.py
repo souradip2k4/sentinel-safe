cad19850fce3cfb4e4744598d2322d4804f1b51c
souradip2k4 <22052939@kiit.ac.in>
2025-01-17 20:14:12 +0530

    Containerised client docker file

--- model/main.py
+++ model/main.py
@@ -3,15 +3,14 @@ import time
 from sentiment import sentiment
 from locMetrics import update_locmetrics
 
-
 # schedule.every(30).minutes.do(sentiment)
 # schedule.every(1).hours.do(update_locmetrics)
 
 
 # print("Main.py Running....")
 while True:
-    # schedule.run_pending()
-    sentiment()
-    time.sleep(10)
-    print("Sentiment Done....")
+  # schedule.run_pending()
+  sentiment()
+  time.sleep(10)
+  print("Sentiment Done....")
 # print("Script Stopped....")
